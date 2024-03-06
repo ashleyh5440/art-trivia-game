020b2cbd579a93eeb02a73f720cb5d26cf204d08
Alexander Egerev <alexandersegerev@gmail.com>
2024-03-05 20:50:00 -0500

    Updated packages; created models

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require('mongoose');
+const scoreSchema = require('./Score');
+
+const userSchema = new Schema ({
+    id: {
+        type: Int,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'You need to use a valid email address!']
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+
+    category: {
+        type: String,
+        required: true,
+    },
+
+    score_value: {
+        type: Int,
+        required: true,
+    },
+    savedScores: [scoreSchema],   
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const Score = require('./Score');
+const User = require('./User');
+
+module.exports = {Score, User};
