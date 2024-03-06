cc7e2551b8c058b8ee6d048db8d26edeaa094d55
Ashley <ashleyh5440@gmail.com>
2024-03-06 16:30:08 -0500

    started working on the questions

--- client/src/components/Categories/index.jsx
+++ client/src/components/Categories/index.jsx
@@ -1,30 +1,73 @@
 import { useState, useEffect } from 'react';
 import '../Categories/style.css'
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 import axios from 'axios';
 
 function Categories() {
     const [selectedCategory, setSelectedCategory] = useState(null);
-
+    const navigate = useNavigate()
     const [currentQuestions, setCurrentQuestions] = useState([]);
 
     const handleCategorySelection = async (categoryId) => {
         try {
-            const response = await axios.get(`https://opentdb.com/api.php?amount=30&category=${categoryId}`);
-            const questions = response.data.results;
-            console.log('trivia questions', questions); 
-            setCurrentQuestions(questions);
+            setSelectedCategory(categoryId);
+            const response = await axios.get(`https://opentdb.com/api.php?amount=30&category=${categoryId}&encode=base64`);
+            const trimData = response.data.results.map((item) => {
+                // decode the question
+                let trimmedQuestion = atob(item.question);
+                let b2bQues = Uint8Array.from(trimmedQuestion, (m) =>
+                  m.codePointAt(0)
+                );
+                let decodedB2BQ = new TextDecoder().decode(b2bQues);
+    
+                // decode correct answer
+                let trimmedCorrect = atob(item.correct_answer);
+                let b2bTrimmedCorrect = Uint8Array.from(trimmedCorrect, (m) =>
+                  m.codePointAt(0)
+                );
+                let decodedB2BATC = new TextDecoder().decode(b2bTrimmedCorrect);
+                // decode all the incorrect answers
+            let trimmedIncorrect = item.incorrect_answers.map((wrongTrim) => {
+                wrongTrim = atob(wrongTrim);
+                let b2bWrongTrim = Uint8Array.from(wrongTrim, (m) =>
+                  m.codePointAt(0)
+                );
+                let decodedB2BA = new TextDecoder().decode(b2bWrongTrim);
+                return decodedB2BA;
+              });
+              const options = [...trimmedIncorrect, decodedB2BATC];
+  
+              // shuffle the positions of the answer options
+              const shuffledOptions = [...options];
+              for (let i = shuffledOptions.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffledOptions[i], shuffledOptions[j]] = [
+                  shuffledOptions[j],
+                  shuffledOptions[i],
+                ];
+              }
+              return {
+                question: decodedB2BQ,
+                correctAnswer: decodedB2BATC,
+                options: shuffledOptions,
+              };
+            });
+             navigate("/game", {state: {questions:trimData, category:selectedCategory}});
         } catch (error) {
             console.error('error getting questions', error);
         }
     };
 
     return (
-        
+        // should I put the event listener on the container?
+        //when the user clicks the button, it should go to Game with the appropriate questions for that category
+        //questions should appear one by one and be timed
+        //remember that some are multiple choice, some are true/false
         <section className="category-buttons">
           <div className="left">
-            <Button variant="primary" onClick={() => handleCategorySelection(25)}>Art</Button>
+            <Button variant="primary" onClick={() => handleCategorySelection({id: 25, title: "Art"})}>Art</Button> 
             <Button variant="primary" onClick={() => handleCategorySelection(10)}>Books</Button>
             <Button variant="primary" onClick={() => handleCategorySelection(18)}>Computers</Button>
             <Button variant="primary" onClick={() => handleCategorySelection(11)}>Film</Button>
@@ -34,17 +77,6 @@ function Categories() {
             <Button variant="primary" onClick={() => handleCategorySelection(17)}>Science & Naure</Button>
             <Button variant="primary" onClick={() => handleCategorySelection(21)}>Sports</Button>
             <Button variant="primary" onClick={() => handleCategorySelection(14)}>TV</Button>
-          </div>
-          <div>
-          {currentQuestions.map(question => (
-                <p>{question.question}</p>
-            ))}
-            {currentQuestions.map(answers => (
-                <p>{answers.correct_answer}</p>
-            ))}
-            {currentQuestions.map(answers => (
-                <p>{answers.incorrect_answers}</p>
-            ))}
           </div>
         </section>
     )
