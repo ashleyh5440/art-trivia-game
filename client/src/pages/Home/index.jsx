14f1021e0763b0607a98ca7180c728f0f09ab1e6
Ashley <ashleyh5440@gmail.com>
2024-03-07 20:32:45 -0500

    added some animations

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,21 +3,19 @@ import Categories from '../../components/Categories';
 import homePic from '../../assets/nerd.gif';
 import '../Home/style.css'
 import 'bootstrap/dist/css/bootstrap.css';
-
-
+import 'animate.css';
 
 function Home() {
     return (
         <section>
             <div>
-                <h1><strong>Know It All</strong></h1>
+                <h1 className="animate__animated animate__slideInDown"><strong>Know It All</strong></h1>
                 <img id="home-pic" src={homePic} />
-                <p>The Trivia Game For Smart-Asses</p>
+                <p className="animate__animated animate__flipInX animate__delay-3s">The Trivia Game For Smart-Asses</p>
             </div>
             <div className="categories">
                 <Categories />
             </div>
-            
         </section>
     );
 };
