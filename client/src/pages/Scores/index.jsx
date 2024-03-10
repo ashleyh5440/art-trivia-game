7e8fc2031fa3b473e46c56f5dce81790222780ba
Alexander Egerev <alexandersegerev@gmail.com>
2024-03-10 12:44:22 -0400

    Updated Scores to use React table

--- client/src/pages/Scores/index.jsx
+++ client/src/pages/Scores/index.jsx
@@ -1,78 +1,123 @@
-import { useState } from 'react';
-import homePic from '../../assets/homePic.jpg';
+import React, { useState, useEffect } from 'react';
+import {useSortBy, useTable} from 'react-table';
 import './style.css'
 
 function Scores() {
+    //instead of using this hardcoded data, have a function to retrieve data from database
+    const data = React.useMemo(
+        () => [ 
+            {
+                name: "Monkey D. Luffy",
+                score: 1
+            },
+            {
+                name: "Roronoa Zoro",
+                score: 3
+            },
+            {
+                name: "Nami",
+                score: 43
+            },
+            {
+                name: "Usopp",
+                score: 323
+            },
+            {
+                name: "Sanji",
+                score: 30
+            },
+
+            
+        ],
+        []
+    )
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "Name                ",
+                accessor: "name",
+            },
+            {
+                Header: "Score",
+                accessor: "score",
+            },
+        ],
+        []
+    )
+
+    
+ const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data },
+    useSortBy)
+
     return (
         <>
-       
-        <section>
-            <div>
-                <h1>HIGH SCORES</h1>
-                <img id="home-pic" src={homePic} />
-            </div>
-            
-            <table id = "table-container"> 
-        <tr>
-            <th id = "name-header">Name</th>
-            <th id = "score-header">Score</th>
-        </tr>
-   {/* THE NAMES AND SCORES ARE TEMPORARY AND WILL BE REPLACED WITH ACTUAL USER NAMES... SOON.
-       WHOEVER IS IN CHARGE OF THE USERNAMES, MAKE IT SO. */}   
-        <tr>
-            <td id = "name">Monkey D. Luffy</td>
-            <td id = "score">1</td>
-        </tr>
+       <section>
+       <div>
+            <h1>HIGH SCORES</h1>
+        </div>
 
-        <tr>
-            <td id = "name">Roronoa Zoro</td>
-            <td id = "score">3</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Nami</td>
-            <td id = "score">43</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Usopp</td>
-            <td id = "score">18</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Black-Leg Sanji</td>
-            <td id = "score">18</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Tony Tony Chopper</td>
-            <td id = "score">91</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Nico Robin</td>
-            <td id = "score">130</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Franky</td>
-            <td id = "score">322</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Brook</td>
-            <td id = "score">381</td>
-        </tr>
-
-        <tr>
-            <td id = "name">Jimbei</td>
-            <td id = "score">876</td>
-        </tr>
+        <div>
+            <table {...getTableProps()} style={{border: 'solid 1px yellow'}}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th 
+                                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                                    style={{
+                                        borderBottom: 'solid 3px red',
+                                        color: 'aquamarine'
+                                    }}
+                                >
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? '🔽'
+                                                : '🔼'
+                                            : ''}
+                                        </span>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td 
+                                        {...cell.getCellProps()}
+                                        style={{
+                                            padding: '1px',
+                                            border: 'solid 1px green',
+                                            color: 'white'
+                                        }}
+                                        >
+                                            {cell.render('Cell')}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
 
             </table>
-  
-            
-        </section>
+        </div>
+    </section>
+
+      
         </>
     );
 };
